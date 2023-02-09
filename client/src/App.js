6d493dc4c171b1b5f435819da260165498fd2a34
rasedul99 <rasedul2199@gmail.com>
2023-02-10 05:10:11 +0600

    header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
 import Layout from "./components/Layout/Layout";
+
+// import "./App.css";
 function App() {
   return (
-    <div className="App">
+    <div>
       <Layout />
     </div>
   );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="bg-[#E4E1D5] pt-[15px] pb-4  text-center text-base">
+      <p className="text-[#2D2929]">
+        Use this banner to inform your users of something important.
+      </p>
+    </div>
+  );
 };
 
 export default Header;

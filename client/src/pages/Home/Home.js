57963b40428ad22419eba3fb3fee633819e3c8fc
rasedul99 <rasedul2199@gmail.com>
2023-02-12 03:10:36 +0600

    banner added in homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import banner from "../../assets/banner/banner.png";
 import Collection from "../../components/Collection/Collection";
 import Slider from "../../components/Slider/Slider";
 
@@ -9,6 +10,16 @@ const Home = () => {
       <div className="my-[100px] px-[100px]">
         <h3 className="mb-12">OUR COLLECTION</h3>
         <Collection />
+      </div>
+      <div className="relative">
+        <img src={banner} className="w-full h-full object-contain" />
+        <div className="flex flex-col gap-12 w-[378px] text-center text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          <h3 className="font-semibold text-[44px]">20% OFF</h3>
+          <p>And extra 50% off sale styles code FORCE20</p>
+          <button className="border border-solid border-white py-3 px-[56px] w-[189px] mx-auto">
+            VIEW ALL
+          </button>
+        </div>
       </div>
     </div>
   );
